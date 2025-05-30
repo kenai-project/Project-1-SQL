9db25c2b854850d46bfaba7bf76c7213fed73d0f
ksnk9154 <ksnk9154@gmail.com>
2025-05-30 16:54:41 +0530

    hm

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: process.env.FRONTEND_URL || "http://localhost:3000",
   credentials: true,
 };
 app.use(cors(corsOptions));
