b93ecf0ec45395c27ea8f0ffb1b4bc6a2ba3ed43
ksnk9154 <ksnk9154@gmail.com>
2025-05-30 16:20:03 +0530

    now 1

--- src/services/spark.service.js
+++ src/services/spark.service.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = '/api/spark';
+const API_URL = "/api/spark";
 
 const runQuery = async (sql) => {
-  const response = await axios.post(`${API_URL}/query`, { sql });
+  const response = await axios.post(API_URL + "/query", { sql });
   return response.data;
 };
 
