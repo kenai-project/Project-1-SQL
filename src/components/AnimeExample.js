48e28c06d9a03317ba321d885e8c33ffdd6059fd
ksnk9154 <ksnk9154@gmail.com>
2025-07-01 10:28:41 +0530

    h

--- src/components/AnimeExample.js
+++ src/components/AnimeExample.js
@@ -1,36 +1,7 @@
-import React, { useEffect, useRef } from 'react';
-import { animate } from 'animejs';
+import React from 'react';
 
 const AnimeExample = () => {
-  const boxRef = useRef(null);
-
-  useEffect(() => {
-    animate({
-      targets: boxRef.current,
-      translateX: 250,
-      rotate: '1turn',
-      backgroundColor: '#FF6347',
-      duration: 2000,
-      loop: true,
-      direction: 'alternate',
-      easing: 'easeInOutSine'
-    });
-  }, []);
-
-  return (
-    <div>
-      <h2>Anime.js Animation Example</h2>
-      <div
-        ref={boxRef}
-        style={{
-          width: '100px',
-          height: '100px',
-          backgroundColor: '#00BFFF',
-          margin: '20px auto'
-        }}
-      />
-    </div>
-  );
+  return <div className="box">Animation goes here</div>;
 };
 
 export default AnimeExample;
